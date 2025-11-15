347a241094e449a375089c5ea12fb8bf7bc1d68d
ANUCIYA123 <anuciya2003@gmail.com>
2025-11-15 17:25:38 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 import App from './components/app'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/EventTicketBookingSystem/">
+    <div>
         <App />
-    </BrowserRouter>
+    </div>
 )
